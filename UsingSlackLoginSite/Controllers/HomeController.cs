b3bdcfb718de03f1d39f98579d7148f19f4fada1
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2015-11-26 17:46:26 +0100

    Added home controller, one anonymous view (/Home/Index) and one secured (/Home/Secure)

---
+++ UsingSlackLoginSite/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace UsingSlackLoginSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Secure()
+        {
+            return View();
+        }
+    }
+}
