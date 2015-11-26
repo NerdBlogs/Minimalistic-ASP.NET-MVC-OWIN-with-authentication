e2533378ae2484ecb28b91d898c363aad74d3ac5
Fredrik Johnsson <fredrik.johnsson7@gmail.com>
2015-11-26 18:04:37 +0100

    Account controller for external login redirect and callback handling added, the user is signed in with the claims received from Slack

---
+++ UsingSlackLoginSite/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace UsingSlackLoginSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login(string returnUrl)
+        {
+            return new ChallengeResult("Slack", Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl }));
+        }
+
+        public ActionResult ExternalLoginCallback(string returnUrl)
+        {
+            var loginInfo = HttpContext.GetOwinContext().Authentication.GetExternalLoginInfo();
+            if (loginInfo.ExternalIdentity.IsAuthenticated)
+            {
+                SignIn();
+                return Redirect(returnUrl);
+            }
+
+            return new HttpUnauthorizedResult();
+        }
+
+        private void SignIn()
+        {
+            var loginInfo = HttpContext.GetOwinContext().Authentication.GetExternalLoginInfo();
+            if (loginInfo.ExternalIdentity.IsAuthenticated)
+            {
+                var manager = HttpContext.GetOwinContext().Authentication;
+                var claims = loginInfo.ExternalIdentity.Claims;
+                manager.SignIn(new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                    IsPersistent = false,
+                    ExpiresUtc = DateTimeOffset.Now.AddMinutes(60)
+                },
+                new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie));
+            }
+        }
+
+        private class ChallengeResult : HttpUnauthorizedResult
+        {
+            public ChallengeResult(string provider, string redirectUri)
+            {
+                LoginProvider = provider;
+                RedirectUri = redirectUri;
+            }
+
+            private string LoginProvider { get; }
+            private string RedirectUri { get; }
+
+            public override void ExecuteResult(ControllerContext context)
+            {
+                var properties = new AuthenticationProperties() { RedirectUri = RedirectUri };
+                context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
+            }
+        }
+    }
+}
